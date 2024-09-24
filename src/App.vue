2c217d97f97c6e48d3ec4f56679988374d8d2e5f
kulkarnianushree <anushreejmkulkarni@gmail.com>
2024-09-24 13:03:41 +0530

    creating basic vue app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+        <h2>My Family</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data:function (){
+        return{
+            family:[
+                {
+                    id:'1',
+                    name:'Anushree Kulkarni',
+                    phone:'9108 821 641',
+                    email:'anushree@gmail.com'
+                },
+                {
+                    id:'2',
+                    name:'Abhinav Kulkarni',
+                    phone:'8147 176 732',
+                    email:'abhinav@gmail.com'
+                },
+                {
+                    id:'3',
+                    name:'Jayaraj Kulkarni',
+                    phone:'9449 768 262',
+                    email:'jayaraj@gmail.com',
+                },
+                {
+                    id:'4',
+                    name:'Usha Kulkarni',
+                    phone:'8867 246 245',
+                    email:'usha@gmail.com'
+                }
+            ]
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
